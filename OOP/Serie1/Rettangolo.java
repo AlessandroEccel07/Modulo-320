7b18a42c4bf227f3414c2b928dad48c85b49b67f
AlessandroEccel07 <ixm775@student.edu.ti.ch>
2025-10-08 13:53:56 +0200

    modifica

--- OOP/Serie1/Rettangolo.java
+++ OOP/Serie1/Rettangolo.java
@@ -13,7 +13,7 @@ public class Rettangolo {
     public void disegno() {
         for (int i = 0; i < (int) latoMaggiore; i++) {
             for (int j = 0; j < (int) latoMinore; j++) {
-                System.out.print("*");
+                System.out.print("X");
             }
             System.out.println();
         }
