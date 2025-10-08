d434130a62ea07a81088e03de3775dfeac6d463e
AlessandroEccel07 <ixm775@student.edu.ti.ch>
2025-10-08 14:18:43 +0200

    dsa

--- OOP/Serie1/Main.java
+++ OOP/Serie1/Main.java
@@ -3,7 +3,7 @@ public class Main {
         Rettangolo myObj = new Rettangolo();
         myObj.latoMaggiore = 5;
         myObj.latoMinore = 5;
-        System.out.println(myObj.latoMaggiore+"x"+myObj.latoMinore+" Perimetro: " + myObj.perimetro() + " Area: " + myObj.area());
+        System.out.println(myObj.latoMaggiore + "x" + myObj.latoMinore + " Perimetro: " + myObj.perimetro() + " Area: " + myObj.area());
         myObj.disegno();
 
     }
